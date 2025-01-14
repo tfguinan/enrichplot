519c7efeb2ebaff44360f5dcf0331ee15d79695c
tfguinan <59532990+tfguinan@users.noreply.github.com>
2025-01-14 13:43:12 +1100

    Update treeplot.R
    Reverse vexpand

--- R/treeplot.R
+++ R/treeplot.R
@@ -577,7 +577,7 @@ treeplot.compareClusterResult <-  function(x,
             
     }
     p <- p + ggtree::hexpand(ratio = hexpand)
-    p + ggtree::vexpand(ratio = 0.2) # More room for xaxis labels
+    p + ggtree::vexpand(ratio = 0.2, direction = -1) # More room for xaxis labels (-1 direction for bottom)
 }
 
 
